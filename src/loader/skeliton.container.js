7fffd2f1467f76b1be4edbae556b351e38e9cc60
santosh kumar <santosh.metikoti@gmail.com>
2021-08-21 18:51:38 +0530

    changes in grid size

--- src/loader/skeliton.container.js
+++ src/loader/skeliton.container.js
@@ -3,6 +3,7 @@ import Skeleton from '@material-ui/lab/Skeleton';
 import { makeStyles } from '@material-ui/core/styles';
 import { Grid } from '@material-ui/core';
 
+
 const useStyles = makeStyles({
     root: {
         // marginTop: 10,
